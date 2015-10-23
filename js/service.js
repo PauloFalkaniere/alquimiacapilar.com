b38612d95c4bbfcee12a4211bc776d16d37a5b3a
paulofalkaniere@gmail.com <paulofalkaniere@gmail.com>
2015-10-23 17:42:31 -0200

    initial commit

---
+++ js/service.js
@@ -0,0 +1,20 @@
+var alquimia = angular.module('alquimia', ['ui.router']);
+alquimia.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+alquimia.config(function($stateProvider, $urlRouterProvider) {
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "views/principal.html"
+    })
+    .state('contato', {
+      url: "/contato",
+      templateUrl: "views/contato.php"
+    })
+});
